11f17d36bef5c9e615a4ab84b59b98969378dfa0
Manindra Singh <manindra23@yahoo.co.in>
2019-01-30 18:11:11 +0530

    Adding events and few checks

--- Permissions.sol
+++ Permissions.sol
@@ -1,39 +1,47 @@
 pragma solidity^0.5.3;
 
 contract Permissions {
+  uint count;
   address owner;
   mapping (address => address[5]) public permissions;
-  uint8 count;
 
   struct MedicalRecord {
     string name;
     address patient;
     address hospital;
-    uint256 admissionDate;
-    uint256 dischargeDate;
-    uint256 visitReason;
+    uint admissionDate;
+    uint dischargeDate;
+    string visitReason;
   }
+
+  event PermissionGranted(address indexed publicAddress, bool result);
+  event PermissionChecked(address indexed publicAddress, bool result);
 
   constructor() public {
     owner = msg.sender;
   }
 
   function grantPermission(address publicAddress) public returns (bool) {
+    bool result = false;
     address[5] storage addressArray = permissions[msg.sender];
-    if(count < 5) {
-        addressArray[count] = publicAddress;
-    }
+    require(count < 5, "Maximum limit reached for granting permission. Not enough space to store any more addresses.");
+    addressArray[count] = publicAddress;
     count = count + 1;
-    return true;
+    result = true;
+    emit PermissionGranted(publicAddress, result);
+    return result;
   }
 
   function hasPermission(address publicAddress) public view returns (bool) {
-    address[5] memory addressArray = permissions[publicAddress];
+    bool result = false;
+    address[5] storage addressArray = permissions[publicAddress];
     for(uint i = 0; i < 5; i++) {
       if(addressArray[i] == msg.sender) {
-        return true;
+        result = true;
+        return result;
       }
     }
-    return false;
+    result = false;
+    return result;
   }
 }
