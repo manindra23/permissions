0fca8c9084d943ce009f2df300206d293122b485
Manindra Singh <manindra23@yahoo.co.in>
2019-01-30 13:34:03 +0530

    Adding functions for granting and checking permissions

--- Permissions.sol
+++ Permissions.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.5.3;
+pragma solidity^0.5.3;
 
 contract Permissions {
   address owner;
-  mapping (address => address[]) public permissions;
+  mapping (address => address[5]) public permissions;
+  uint8 count;
 
   struct MedicalRecord {
     string name;
@@ -15,5 +16,24 @@ contract Permissions {
 
   constructor() public {
     owner = msg.sender;
+  }
+
+  function grantPermission(address publicAddress) public returns (bool) {
+    address[5] storage addressArray = permissions[msg.sender];
+    if(count < 5) {
+        addressArray[count] = publicAddress;
+    }
+    count = count + 1;
+    return true;
+  }
+
+  function hasPermission(address publicAddress) public view returns (bool) {
+    address[5] memory addressArray = permissions[publicAddress];
+    for(uint i = 0; i < 5; i++) {
+      if(addressArray[i] == msg.sender) {
+        return true;
+      }
+    }
+    return false;
   }
 }
