6f5e8b9de1cf2c70a26c3b61c9b1b3b5be542bdd
Manindra Singh <manindra23@yahoo.co.in>
2019-01-30 20:46:46 +0530

    Adding few comments

--- Permissions.sol
+++ Permissions.sol
@@ -2,9 +2,13 @@ pragma solidity^0.5.3;
 
 contract Permissions {
   address owner;
+  //Mapping between granter address and receiver map which in turn
+  //contains mapping between receiver address and his/her permission
   mapping (address => mapping (address => bool)) public permissions;
+  //Mapping between receiver address and his/her permission
   mapping (address => bool) receiverPermissionsGlobal;
 
+  //Entity to be accessed based on permission
   struct MedicalRecord {
     string name;
     address hospitalAddress;
@@ -13,6 +17,7 @@ contract Permissions {
     string visitReason;
   }
 
+  //Mapping between receiverAddress and his/her MedicalRecord
   mapping (address => MedicalRecord) medicalRecordMap;
 
   constructor() public {
